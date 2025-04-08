2dfdf9a3c4ceb83c298e051477d27b0b593ab27e
Yue Wu <akumatus@gmail.com>
2025-04-08 15:11:34 +0800

    refactor(core): ai session create

--- packages/frontend/core/src/blocksuite/ai/actions/edgeless-handler.ts
+++ packages/frontend/core/src/blocksuite/ai/actions/edgeless-handler.ts
@@ -207,7 +207,7 @@ function actionToStream<T extends keyof BlockSuitePresets.AIActions>(
           }
 
           // @ts-expect-error TODO(@Peng): maybe fix this
-          stream = action(options);
+          stream = await action(options);
           if (!stream) return;
           yield* stream;
         },
